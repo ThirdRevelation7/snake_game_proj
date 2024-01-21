4f661686b5db5229bc4fa5c9bac368f5890679e5
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2024-01-21 17:28:36 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+
+def snake_direction():
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    snake_direction()
+
+
+screen.exitonclick()
